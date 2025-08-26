d35f2cf8e6b887f5279109274072cc73d350dc54
Cynthiaheckphd <cmporter2@uncg.edu>
2025-08-26 09:59:54 -0400

    edit: script tutorial

--- code/script_tutorial.R
+++ code/script_tutorial.R
@@ -1,3 +1,6 @@
+
+
+# example code ------------------------------------------------------------
 x <- c(1, 2)
 x
 
@@ -12,3 +15,27 @@ mean(x)
 
 ## estimate SD
 sd(x)
+
+## quick coding
+z <- c(1, 2, 3)
+
+
+# exercise ----------------------------------------------------------------
+
+# create a vector with length 10 assign it to z
+z <- c(1:10)
+length(z)
+
+# create a numeric matrix with 2 rows x 2 columns
+
+m <- matrix(1:4, nrow = 2, ncol = 2)
+m
+m <- cbind(c(1, 2), c(3, 4))
+m
+m <- rbind(c(1, 2), c(3, 4))
+m
+
+# data frame
+
+df0 <- data.frame(name = c("Smith", "John", "Kate", "Cynthia"), height = c(154, 170, 156, 175))
+df0
